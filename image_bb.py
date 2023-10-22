8f001da5b73c63f56d19fd08c63d45d93a1d4788
Chris <cx72@cornell.edu>
2023-10-22 15:57:24 -0400

    saving bounding boxes to images folder

--- image_bb.py
+++ image_bb.py
@@ -1,4 +1,6 @@
 import cv2
+import os
+from datetime import datetime
 
 imgPath = "test.jpeg"
 img = cv2.imread(imgPath)
@@ -7,8 +9,15 @@ g_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 haar_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 faces_rect = haar_cascade.detectMultiScale(g_img, scaleFactor=1.1, minNeighbors=6) 
 
-for (x, y, w, h) in faces_rect: 
+os.makedirs('images', exist_ok=True)
+
+name = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+os.makedirs('images/{}'.format(name))
+
+for i, (x, y, w, h) in enumerate(faces_rect): 
     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), thickness=3) 
-  
+    roi = img[y:y+h, x:x+w]
+    cv2.imwrite('images/{}/roi{}.jpg'.format(name, i), roi)
+
 cv2.imshow('bounding-boxes', img) 
 cv2.waitKey(0)
--- video_bb.py
+++ video_bb.py
@@ -1,7 +1,17 @@
 import cv2
+import os 
+from datetime import datetime
 
 webcam = cv2.VideoCapture(0)
 haar_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+
+os.makedirs('images', exist_ok=True)
+
+name = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+os.makedirs('images/{}'.format(name))
+
+image_count = 0
 
 while True:
     (_, img) = webcam.read()
@@ -10,6 +20,8 @@ while True:
     for (x, y, w, h) in faces_rect: 
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), thickness=3) 
         face = img[y:y + h, x:x + w] 
+        cv2.imwrite('images/{}/roi{}.jpg'.format(name, image_count), face)
+        image_count += 1
 
     cv2.imshow('bounding-boxes', img)
     cv2.waitKey(0)
