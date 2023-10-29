b083b2debe554ef00e9b5c0b31c2646d144060e2
Emily Wang <esw77@cornell.edu>
2023-10-29 14:11:15 -0400

    Added clang checker

---
+++ ci/clang-format-checker.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+## Open all modified files in the current branch and run clang-format on them.
+import subprocess
+
+# Get the list of modified files. from git
+modified_files = subprocess.check_output(
+    "find src/ -iname *.h -o -iname *.cpp",
+    shell=True,
+).splitlines()
+
+# convert to list of strings
+modified_files = [f.decode("utf-8") for f in modified_files]
+
+for f in modified_files:
+  result = subprocess.run(["clang-format", "--dry-run", "--Werror", f])
+  if result.returncode:
+    exit(1)
+
+exit(0)
