5cf29b8b87a5e5d8aa4013988605b2c08d4f6580
Chris <cx72@cornell.edu>
2023-11-12 15:18:27 -0500

    Motion detection for comparing 2 pictures

---
+++ build/CMakeFiles/motion_detection.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/chris/Documents/CDS/edge-ml/utils/motion_detection.cpp" "CMakeFiles/motion_detection.dir/motion_detection.cpp.o" "gcc" "CMakeFiles/motion_detection.dir/motion_detection.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/motion_detection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/motion_detection.dir/motion_detection.cpp.o"
+  "CMakeFiles/motion_detection.dir/motion_detection.cpp.o.d"
+  "motion_detection"
+  "motion_detection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/motion_detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
