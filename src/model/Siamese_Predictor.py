5c5831caa43bffd214b8c3def0379fdb3ada08b8
srishagaur <sg2455@cornell.edu>
2023-12-01 17:30:47 -0500

    Simple camera writes to file and triggers prediction

--- src/model/Siamese_Predictor.py
+++ src/model/Siamese_Predictor.py
@@ -3,7 +3,9 @@ import tensorflow as tf
 from tensorflow.keras.applications import resnet
 import argparse
 from tensorflow.keras import metrics
-
+import time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
 
 def preprocess_image(filename):
     """
@@ -18,31 +20,54 @@ def preprocess_image(filename):
     image = tf.image.resize(image, target_shape)
     return image
 
-
-# Initialize parser
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--filename", type=str, help="Path to the input image file (e.g., img.jpg)"
-)
-
-args = parser.parse_args()
-
 # load the model
-embedding = tf.keras.models.load_model("siamese_feature.h5")
+embedding = tf.keras.models.load_model("siamese_feature.h5", compile=False)
 
+def get_similarity_score(img_path):
+    print("Wait a bit for image to write?")
+    time.sleep(5);
+    # TODO: lol, fix this
+    print("After 5 seconds")
+    david_1 = preprocess_image(img_path)
+    david_2 = preprocess_image("david_base.jpg")
+    # add a dimension to the tensor
+    david_1 = tf.expand_dims(david_1, axis=0)
+    david_2 = tf.expand_dims(david_2, axis=0)
 
-david_1 = preprocess_image(args.filename)
-david_2 = preprocess_image("david_base.jpg")
-# add a dimension to the tensor
-david_1 = tf.expand_dims(david_1, axis=0)
-david_2 = tf.expand_dims(david_2, axis=0)
+    anchor_embedding, positive_embedding = (
+        embedding(resnet.preprocess_input(david_1)),
+        embedding(resnet.preprocess_input(david_2)),
+    )
 
-anchor_embedding, positive_embedding = (
-    embedding(resnet.preprocess_input(david_1)),
-    embedding(resnet.preprocess_input(david_2)),
-)
+    cosine_similarity = metrics.CosineSimilarity()
 
-cosine_similarity = metrics.CosineSimilarity()
+    positive_similarity = cosine_similarity(anchor_embedding, positive_embedding)
+    print("Similarity score: ", positive_similarity.numpy())
 
-positive_similarity = cosine_similarity(anchor_embedding, positive_embedding)
-print("Similarity score: ", positive_similarity.numpy())
+# set up on created
+def on_created(event):
+    print(f"hey, {event.src_path} has been created!")
+    get_similarity_score(event.src_path)
+
+# set up watchdog
+patterns = ["*"]
+ignore_patterns = None
+ignore_directories = False
+case_sensitive = True
+my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+my_event_handler.on_created = on_created
+
+# create observer
+path = "../../Images"
+go_recursively = True
+my_observer = Observer()
+my_observer.schedule(my_event_handler, path, recursive=go_recursively)
+
+# run observer
+my_observer.start()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    my_observer.stop()
+    my_observer.join()
