93b1c616fbd60e04831a7291cdc90c1d451f883d
Emily Wang <esw77@cornell.edu>
2023-12-02 18:11:31 -0500

    new siamese network

--- src/model/Siamese_Predictor.py
+++ src/model/Siamese_Predictor.py
@@ -8,18 +8,27 @@ from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 import threading
 
+WIDTH = HEIGHT = 105
+CEELS = 1
+
+
 def preprocess_image(filename):
     """
     Load the specified file as a JPEG image, preprocess it and
     resize it to the target shape.
     """
-    target_shape = (200, 200)
+    anchor = Image.open("david_base.jpg")
+    img = Image.open("David_Han_01.jpg")
 
-    image_string = tf.io.read_file(filename)
-    image = tf.image.decode_jpeg(image_string, channels=3)
-    image = tf.image.convert_image_dtype(image, tf.float32)
-    image = tf.image.resize(image, target_shape)
+    # resize images to 105 x 105
+    anchor = anchor.resize((WIDTH, HEIGHT))
+    img = img.resize((WIDTH, HEIGHT))
+    # make black white and reduce channels to 1
+    anchor = anchor.convert("L")
+    img = img.convert("L")
+
     return image
+
 
 # load the model
 embedding = tf.keras.models.load_model("siamese_feature.h5", compile=False)
@@ -29,24 +38,26 @@ print("Model is done loading")
 david_2 = preprocess_image("david_base.jpg")
 david_2 = tf.expand_dims(david_2, axis=0)
 
+
 def get_similarity_score(img_path):
-    time.sleep(0.5);
+    time.sleep(0.5)
     david_1 = preprocess_image(img_path)
-    
+
     # add a dimension to the tensor
     david_1 = tf.expand_dims(david_1, axis=0)
-    
+
     # get embeddings
     anchor_embedding, positive_embedding = (
         embedding(resnet.preprocess_input(david_1)),
         embedding(resnet.preprocess_input(david_2)),
     )
-    
+
     # get similarity score
     cosine_similarity = metrics.CosineSimilarity()
 
     positive_similarity = cosine_similarity(anchor_embedding, positive_embedding)
     print(f"Similarity score for {img_path}: ", positive_similarity.numpy())
+
 
 # set up on created
 def on_created(event):
@@ -54,12 +65,15 @@ def on_created(event):
     model_thread = threading.Thread(target=lambda: get_similarity_score(event.src_path))
     model_thread.start()
 
+
 # set up watchdog
 patterns = ["*"]
 ignore_patterns = None
 ignore_directories = False
 case_sensitive = True
-my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+my_event_handler = PatternMatchingEventHandler(
+    patterns, ignore_patterns, ignore_directories, case_sensitive
+)
 my_event_handler.on_created = on_created
 
 # create observer
