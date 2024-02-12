d368843211d7a3a72c857bbd9032e3c75b5d337a
Bryant Park <bryantpark04@gmail.com>
2024-02-12 00:59:02 -0500

    Run black via pre-commit

--- src/model/EdgeTraining.py
+++ src/model/EdgeTraining.py
@@ -11,9 +11,7 @@ loss_type = "binary_crossentropy"
 
 data_path = "realtime"
 
-train_path = os.path.join(
-        data_path, "train.pickle"
-    )  # A path for the train file
+train_path = os.path.join(data_path, "train.pickle")  # A path for the train file
 
 siamese = SiameseNetwork(
     seed=seed,
@@ -27,21 +25,21 @@ siamese = SiameseNetwork(
 )
 
 loader = DataLoader(
-            width=WIDTH,
-            height=HEIGHT,
-            cells=CEELS,
-            data_path=data_path,
-            output_path=train_path,
-        )
+    width=WIDTH,
+    height=HEIGHT,
+    cells=CEELS,
+    data_path=data_path,
+    output_path=train_path,
+)
 loader.load(set_name="train")
 
 siamese.fit(
-        weights_file="weights/weights.h5",
-        train_path=train_path,
-        validation_size=0.2,
-        batch_size=32,
-        epochs=2,
-        early_stopping=True,
-        patience=5,
-        min_delta=0.1,
-    )
+    weights_file="weights/weights.h5",
+    train_path=train_path,
+    validation_size=0.2,
+    batch_size=32,
+    epochs=2,
+    early_stopping=True,
+    patience=5,
+    min_delta=0.1,
+)
